90a085fe1d9aab269d92726d2d3e052c6b9bc8de
PH-Chen1126 <xavier881126@gmai.com>
2025-04-14 20:11:11 +0800

    Add attempt countdown message after wrong password input

--- bios_sim.py
+++ bios_sim.py
@@ -9,3 +9,22 @@ def verify_password(input_password, correct_password="abc123", tries_left=3):
         return "ACCESS GRANTED"
     else:
         return "WRONG PASSWORD"
+
+if __name__ == "__main__":
+    tries = 3
+    correct_pw = "abc123"
+    
+    while tries > 0:
+        pw = input("Please enter your password: ")
+        tries -= 1
+        result = verify_password(pw, correct_password=correct_pw, tries_left=tries)
+        
+        if result == "ACCESS GRANTED":
+            print(result)
+            break
+        elif tries == 0:
+            print("LOCKED")
+            print("System Locked!")
+            break
+        else:
+            print(f"WRONG PASSWORD ({tries} attempts left)")
